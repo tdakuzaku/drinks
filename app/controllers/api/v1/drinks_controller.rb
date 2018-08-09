df1924b6ff181cd3e88156fdd081d408bc6eb74e
Tiago Dakuzaku <tdakuzaku@gmail.com>
2018-08-09 07:14:21 -0300

    Search results list

--- app/controllers/api/v1/drinks_controller.rb
+++ app/controllers/api/v1/drinks_controller.rb
@@ -5,6 +5,12 @@ module Api
 				drinks = Drink.all.order(:created_at)
 				render json: {status: 'SUCCESS', message:'Lista de Drinks', data:drinks}, status: :ok
 			end
+			def show
+				# SQL Injection ?
+				search =  params[:id] + '%'
+				drinks = Drink.where("lower(name) like ?", search.downcase)
+				render json: {status: 'SUCCESS', message:'Resultado da busca', data:drinks}, status: :ok
+			end
 		end
 	end
 end
