76991fa944a6368fe25ecfbe250264febf9aafc8
Tiago Dakuzaku <tdakuzaku@gmail.com>
2018-08-09 21:04:55 -0300

    Refactoring API routes

--- app/controllers/api/v1/drinks_controller.rb
+++ app/controllers/api/v1/drinks_controller.rb
@@ -2,14 +2,17 @@ module Api
 	module V1
 		class DrinksController < ApplicationController
 			def index
-				drinks = Drink.all.order(:created_at)
-				render json: {status: 'SUCCESS', message:'Lista de Drinks', data:drinks}, status: :ok
+				drinks = Drink.all.order(:created_at).limit(10).offset(10)
+				render json: {status: 'SUCCESS', data: drinks}, status: :ok
 			end
-			def show
+			def total
+				render json: {status: 'SUCCESS', data: Drink.count}, status: :ok
+			end
+			def search
 				# SQL Injection!!!
-				search =  params[:id] + '%'
-				drinks = Drink.where("lower(name) like ?", search.downcase)
-				render json: {status: 'SUCCESS', message:'Resultado da busca', data:drinks}, status: :ok
+				search =  params[:query] + '%'
+				drinks = Drink.where("lower(name) like ?", search.downcase).order(name: :asc).limit(10)
+				render json: {status: 'SUCCESS', data: drinks}, status: :ok
 			end
 			def recommend
 				margin = 10
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
 
   namespace 'api' do
   	namespace 'v1' do
-  		resources :drinks, only: [:index, :show]
+  		resources :drinks, only: [:index]
   	end
   end
 
   root to: 'drinks#index'
 
+  get 'api/v1/drinks/total/', :to => 'api/v1/drinks#total'
+  get 'api/v1/drinks/search/:query', :to => 'api/v1/drinks#search'
   post 'api/v1/drinks/recommend/', :to => 'api/v1/drinks#recommend'
 
   resources :home, only: %i[index]
